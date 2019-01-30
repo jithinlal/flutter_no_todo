1e6b30f370081c6d56d8c550526792c316d36434
jithinlal <jithtitan007@gmail.com>
2019-01-30 23:06:37 +0530

    database base commit

--- lib/ui/notodo_screen.dart
+++ lib/ui/notodo_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:todo/model/nodo_item.dart';
+import 'package:todo/util/database_client.dart';
 
 class NoToDoScreen extends StatefulWidget {
   @override
@@ -6,6 +8,16 @@ class NoToDoScreen extends StatefulWidget {
 }
 
 class _NoToDoScreenState extends State<NoToDoScreen> {
+  final TextEditingController _textEditingController =
+      new TextEditingController();
+  var db = new DatabaseHelper();
+  void _handleSubmit(String text) async {
+    _textEditingController.clear();
+    NoDoItem noDoItem = new NoDoItem(text, DateTime.now().toIso8601String());
+    int savedItemId = await db.saveItem(noDoItem);
+    print('Item saved with id: $savedItemId');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,5 +34,40 @@ class _NoToDoScreenState extends State<NoToDoScreen> {
     );
   }
 
-  void _showFormDialog() {}
+  void _showFormDialog() {
+    var alert = AlertDialog(
+      content: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              controller: _textEditingController,
+              autofocus: true,
+              decoration: InputDecoration(
+                  labelText: 'Item',
+                  hintText: 'eg. Dont buy stuff',
+                  icon: Icon(Icons.note_add)),
+            ),
+          )
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+            onPressed: () {
+              _handleSubmit(_textEditingController.text);
+              _textEditingController.clear();
+            },
+            child: Text('Save')),
+        FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Cancel'))
+      ],
+    );
+    showDialog(
+        context: context,
+        builder: (_) {
+          return alert;
+        });
+  }
 }
